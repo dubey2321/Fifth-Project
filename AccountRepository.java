fdd7bd717fe92df239be45b9ea2a4b122ebc7e16
dubey2321 <77892396+dubey2321@users.noreply.github.com>
2021-01-24 22:25:23 +0530

    Add files via upload

---
+++ AccountRepository.java
@@ -0,0 +1,7 @@
+package com.capgemini.repository;
+import com.capgemini.beans.Account;
+
+public interface AccountRepository {
+	boolean save(Account account);
+	Account searchAccount(int accountNumber);
+}
